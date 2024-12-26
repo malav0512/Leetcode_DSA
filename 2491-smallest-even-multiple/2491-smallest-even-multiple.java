beb8d448202ea913fbcab6be86d00d768a347d70
Malav Modi <111265273+malav0512@users.noreply.github.com>
2024-12-27 01:24:34 +0530

    Time: 0 ms (100%), Space: 40.8 MB (17.5%) - LeetHub

---
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int smallestEvenMultiple(int n) {
+        int ans=0;
+        if(n%2==0){
+            ans=n;}
+            else{
+        ans=n*2;
+            }
+        
+        return ans;
+    }
+}
